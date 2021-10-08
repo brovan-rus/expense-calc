c74f7f8fcf3307cf41d51a620591a52f2732e58a
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-08 06:04:22 +0300

    feat: add list edit function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from 'react';
+import { FC, useState, useEffect, useCallback } from 'react';
 import cn from 'classnames/bind';
 
 import localStorageHandler from '../../utils/LocalStorageHandler';
@@ -6,14 +6,19 @@ import { Container } from '../Container';
 import { Form } from '../Form';
 import { List } from '../List';
 import styles from './App.module.css';
+import { IChange } from '../List';
 
 const cx = cn.bind(styles);
+
 export interface IItem {
   [key: string]: string;
 }
 
 export const App: FC = () => {
   const [itemsList, setItemsList] = useState<IItem[]>([]);
+  useEffect(() => {
+    setItemsList(localStorageHandler.getFromLocalStorage('itemList'));
+  }, []);
 
   const handleAddItem = (formValues: { [index: string]: string }) => {
     const newItem: IItem = { [formValues.itemName]: formValues.itemPrice };
@@ -22,9 +27,25 @@ export const App: FC = () => {
     localStorageHandler.saveToLocalStorage('itemList', newItemsList);
   };
 
+  const handleChangeList = (changes: IChange[]) => {
+    changes.forEach((change) => {
+      setItemsList((state) =>
+        state.map((item, i) => {
+          if (parseInt(change.index) === i && change.prop === 'itemName') {
+            return { [change.name]: Object.values(item)[0] };
+          } else if (parseInt(change.index) === i && change.prop === 'itemPrice') {
+            return { [Object.keys(item)[0]]: change.name };
+          } else {
+            return item;
+          }
+        }),
+      );
+    });
+  };
+
   useEffect(() => {
-    setItemsList(localStorageHandler.getFromLocalStorage('itemList'));
-  }, []);
+    localStorageHandler.saveToLocalStorage('itemList', itemsList);
+  }, [itemsList]);
 
   return (
     <div className={cx('app')}>
@@ -35,7 +56,7 @@ export const App: FC = () => {
         </Container>
         {itemsList.length > 0 && (
           <Container>
-            <List itemsList={itemsList} />
+            <List itemsList={itemsList} onChangeList={handleChangeList} />
           </Container>
         )}
       </div>
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,34 +1,61 @@
-import { FC } from 'react';
+import { FC, FormEvent } from 'react';
 import cn from 'classnames/bind';
 import { IItem } from '../App';
 import { Input } from '../Input';
+import { useForm } from '../../utils/useForm';
 
 import styles from './List.module.css';
 import { Button } from '../Button';
 
 const cx = cn.bind(styles);
 
+export interface IChange {
+  index: string;
+  prop: string;
+  name: string;
+}
 interface IList {
   itemsList: IItem[];
+  onChangeList: (changes: IChange[]) => void;
 }
 
-export const List: FC<IList> = ({ itemsList }) => {
+export const List: FC<IList> = ({ itemsList, onChangeList }) => {
+  const form = useForm();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const changes: IChange[] = [];
+    Object.keys(form.values).forEach((key, i) => {
+      changes[i] = {
+        index: key.split('_')[1],
+        prop: key.split('_')[0],
+        name: form.values[key],
+      };
+    });
+    onChangeList(changes);
+    form.resetForm();
+  };
+
   return (
     <ul className={cx('list')}>
       {itemsList.map((item, i) => (
         <li className={cx('element')} key={i}>
-          <form className={cx('form')}>
+          <form className={cx('form')} onSubmit={handleSubmit}>
             <Input
-              value={Object.keys(item).toString()}
-              name={i.toString()}
+              placeholder={Object.keys(item).toString()}
+              value={form.values[`itemName_${i}`] || ''}
+              name={`itemName_${i}`}
               view="secondary"
+              onChange={form.handleChange}
             />
             <div className={cx('priceContainer')}>
               <Input
-                value={Object.values(item).toString()}
-                name={i.toString()}
-                view="secondary"
+                placeholder={Object.values(item).toString()}
+                value={form.values[`itemPrice_${i}`] || ''}
+                name={`itemPrice_${i}`}
                 align="right"
+                onChange={form.handleChange}
+                view="secondary"
               />
               <span className={cx('currency')}>р.</span>
             </div>
